fdd4bb6f6071587b778365149df690ac6a5cb80e
Hector Sam <hfsam88@gmail.com>
2012-06-10 19:07:16 -0430

    subo un archivo y lo coloca por pantalla xD

--- proyecto/src/proyecto/Proyecto.java
+++ proyecto/src/proyecto/Proyecto.java
@@ -15,5 +15,7 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cliente cliente = new Cliente();
+        cliente.setVisible(true);
     }
 }
